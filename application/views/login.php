ace8baa276f3e0af1e6e0111b88e23bf946d88d0
creattivehardbeat1 <creativehardbeat1@gmail.com>
2017-11-14 13:45:50 +0700

    Update port database

--- application/views/login.php
+++ application/views/login.php
@@ -60,7 +60,7 @@
               <button type="submit1" class="btn btn-primary btn-block btn-flat">Sign Up</button>
             </div>
             <div class="col-xs-offset-4 col-xs-4">
-              <p class="btn btn-primary btn-block btn-flat">Don't have an account? <a href="<?php echo base_url(); ?>users/registration">Register here</a></p>
+              <p class="">Don't have an account? <a href="<?php echo base_url(); ?>users/registration">Register here</a></p>
             </div>
 			
 
